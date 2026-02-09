bc6f4907ea7c9151fa3a970c82d5aee36d75ddc2
Berat Yilmaz <117680393+brt-yilmaz@users.noreply.github.com>
2026-02-09 23:47:39 +0100

    add: left join

--- POSTGRES/joining.sql
+++ POSTGRES/joining.sql
@@ -1,3 +1,5 @@
+
+--------------------------- Inner Join
 SELECT
     users.name,    -- full table names (no aliases) makes the source clear
     posts.title,
@@ -8,3 +10,29 @@ FROM
 INNER JOIN users   -- 'INNER' is the default, but writing it explicitly is a best practice
     ON posts.user_id = users.id -- Joins rows based on matching keys
 LIMIT 10;
+
+
+---------------------- Left join
+SELECT
+    users.name,    -- This column will always be populated because 'users' is the Left Table
+    posts.title    -- This column will be NULL if the user has not written a post
+FROM
+    users          -- LEFT TABLE: get all users 
+LEFT JOIN posts    -- RIGHT TABLE: only matched ones
+    ON users.id = posts.user_id
+ORDER BY
+    users.id;
+
+
+SELECT
+    users.name,
+    users.email
+FROM
+    users          -- We start with all users
+LEFT JOIN posts 
+    ON users.id = posts.user_id
+WHERE
+    posts.id IS NULL; 
+                      -- Since LEFT JOIN returns NULL for non-matching rows, 
+                      -- only the users WITHOUT posts.
+---------------------- 
