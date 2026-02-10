b5f32d896b9020a4d0caf073a99d33b80adda68b
Berat Yilmaz <117680393+brt-yilmaz@users.noreply.github.com>
2026-02-10 09:46:00 +0100

    add: union

--- POSTGRES/joining.sql
+++ POSTGRES/joining.sql
@@ -38,3 +38,72 @@ WHERE
 
 ---------------------  Right Join
 ---------------------- Just use left join, no ninja code, no show off
+
+---------------------- Full Outer Join
+
+SELECT
+    employees.name AS employee_name,
+    projects.title AS project_title,
+    employees.id AS emp_id,
+    projects.id AS proj_id
+FROM
+    employees
+FULL OUTER JOIN projects 
+    ON employees.project_id = projects.id;
+
+/*
+  | employee_name | project_title      | emp_id | proj_id 
+  |---------------|--------------------|--------|---------
+  | John Doe      | Mobile App Revamp  | 101    | 50      
+  | Jane Smith    | Website Migration  | 102    | 51      
+  | Mike Ross     | NULL               | 103    | NULL    
+  | NULL          | AI Integration     | NULL   | 52      
+  
+*/
+
+--------------- handle orphaned data
+SELECT
+    posts.title AS post_title,
+    posts.content AS post_body,
+    -- COALESCE Function: Returns the first NON-NULL value in the list.
+    -- If users.name is NULL (because the user was deleted), it returns 'Unknown User'.
+    COALESCE(users.name, 'Unknown User') AS author_name,
+    posts.user_id
+FROM
+    posts              -- LEFT TABLE (Child): We want to see ALL posts.
+LEFT JOIN users        -- RIGHT TABLE (Parent): We try to find the matching user.
+    ON posts.user_id = users.id;
+
+/*
+  | post_title       | post_body          | author_name    
+  |------------------|--------------------|----------------
+  | SQL Tutorial     | Intro to Select... | John Doe       
+  | React Basics     | Components are...  | Jane Smith     
+  | Legacy Thread    | Old discussion...  | Unknown User   
+  */
+
+-------------------------- union
+
+SELECT 
+    name AS contact_name,
+    email AS contact_email,
+    'Customer' AS source_type  
+FROM 
+    customers
+
+UNION  -- 'UNION' removes duplicates. Use 'UNION ALL' to keep duplicates
+
+SELECT 
+    name,
+    email,
+    'Supplier' AS source_type
+FROM 
+    suppliers;
+/*
+| contact_name   | contact_email      | source_type 
+|----------------|--------------------|-------------
+| Alice Johnson  | alice@gmail.com    | Customer    
+| Bob Smith      | bob@yahoo.com      | Customer    
+| TechCorp Inc.  | sales@techcorp.com | Supplier    
+| Office Depot   | contact@office.com | Supplier    
+*/
